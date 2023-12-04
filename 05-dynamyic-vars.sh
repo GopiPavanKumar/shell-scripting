5285d11d6c4d9d5032f3590da2294443bccd714c
Gopi Pavan <thotagopipavankumar@gmail.com>
2023-12-05 00:27:32 +0530

    Auto push

--- 05-dynamyic-vars.sh
+++ 05-dynamyic-vars.sh
@@ -2,5 +2,5 @@
 
 DATE="17JULY2023"
 NO_OF_SESSIONS=$(who|wc -l)
-echo -e "Good Morning , Todays date is \e[32m $DATE \e[0m"
-echo -e "Total Number of Connected Sessions : \e[32m $NO_OF_SESSIONS \e[0m"
+echo -e "Good Morning , Todays date is \e[31m $DATE \e[0m"
+echo -e "Total Number of Connected Sessions : \e[31m $NO_OF_SESSIONS \e[0m"
