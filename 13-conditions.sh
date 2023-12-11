29046eb453df9fee1ae6d040c31d0966433b0787
Gopi Pavan <thotagopipavankumar@gmail.com>
2023-12-11 19:58:10 +0530

    B56 Auto Push

--- 13-conditions.sh
+++ 13-conditions.sh
@@ -26,6 +26,6 @@ case $ACTION in
         ;;
     *)
         echo -e "\e[35m Valid options are start or stop or restart only \e[0m"
-        echo -e "\e[33m Example Usage: \e[0m bash script.sh start"
-
+        echo -e "\e[33m Example Usage: \e[0m \n\t\t bash script.sh start"
+        
 esac
