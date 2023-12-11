9a7940f454f43b41a532266f7184a774a2d58c9e
Gopi Pavan <thotagopipavankumar@gmail.com>
2023-12-11 19:43:11 +0530

    B56 Auto Push

---
+++ 13-conditions.sh
@@ -0,0 +1,31 @@
+#!/bin/bash/
+
+# Conditions helps us to execure something only if SOME-FACTOR is true or false.
+
+
+# Syntax of case
+
+# case $var in 
+#     opt1) commands-x ;;
+#     opt2) commans-y ;;
+# esac
+
+
+case $ACTION in 
+    start)
+        echo -e "\e[32m Starting shipping service \e[0m"
+        exit 0
+        ;;
+    stop)
+        echo -e "\e[31m Stopping shipping service \e[0m"
+        exit 1
+        ;;
+    restart)
+        echo -e "\e[33m Restarting shipping service \e[0m"
+        exit 3
+        ;;
+    *)
+        echo -e "\e[35m Valid options are start or stop or restart only \e[0m"
+        echo -e "\e[33m Example Usage: \e[0m bash script.sh start"
+
+esac
