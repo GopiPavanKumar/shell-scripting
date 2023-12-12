7dea77c0b0d528627256640f5f4a141a1e335918
Gopi Pavan <thotagopipavankumar@gmail.com>
2023-12-12 19:30:15 +0530

    B56 Auto Push

--- 15.ifelse.sh
+++ 15.ifelse.sh
@@ -8,12 +8,12 @@ if [ "$ACTION" == "start" ] ; then
    echo -e "\e[32m Starting shipping service \e[0m"
    exit 0
 elif [ "$ACTION" == "stop" ] ; then 
-   echo -e "\e[33m Stopping shipping service \e[0m"
+   echo -e "\e[31m Stopping shipping service \e[0m"
    exit 1
-elif [ "$ACTION" == "stop" ] ; then 
+elif [ "$ACTION" == "restart" ] ; then 
    echo -e "\e[33m Restarting shipping service \e[0m"
    exit 1
-
 else 
    echo -e "\e[35m Valid option are start or stop or restart only  \e[0m"
    exit 3
+fi
