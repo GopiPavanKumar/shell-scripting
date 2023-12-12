0a37d2af1acfe2a1ffd310f0ab391282cebc132f
Gopi Pavan <thotagopipavankumar@gmail.com>
2023-12-12 20:11:34 +0530

    B56 Auto Push

--- 16-loops.sh
+++ 16-loops.sh
@@ -5,11 +5,14 @@
 # There are 2 major types of loops ;
 #     1) For loop       ( When you know something to be executed n number of time, we use for loop)
 #     2) While loop     ( When you don't know something to be executed n number of times, we use while loop)
+ 
+#echo value is 10
+#echo value is 20
+#echo value is 30
+#echo value is 40
+#echo value is 50
 
-# for loop syntax 
-
-echo value is 10
-echo value is 20
-echo value is 30
-echo value is 40
-echo value is 50
+# for loop syntax
+for i in 10 20 30 40 50 ; do
+    echo "Values from the loops are $i"
+done
