113d154187c352955e61994cc9e29493881fd94a
Gopi Pavan <thotagopipavankumar@gmail.com>
2024-01-15 19:30:36 +0530

    B56 Auto push

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -29,7 +29,7 @@ echo -n " Installing $COMPONENT :"
 yum install -y mongodb-org  &>> ${LOGFILE}
 stat $?
 
-echo -n "Enabling $COMPONENT visibilty :
+echo -n "Enabling $COMPONENT visibilty :"
 sed -i -e 's/127.0.0.1/0.0.0.0/' mongod.conf
 stat $?
 
