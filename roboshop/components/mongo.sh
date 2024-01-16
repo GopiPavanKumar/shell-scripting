91bd0d132ad68ad9c6e89dffacfaa607800c1890
Gopi Pavan <thotagopipavankumar@gmail.com>
2024-01-16 19:55:15 +0530

    B56 Auto push

--- roboshop/components/mongo.sh
+++ roboshop/components/mongo.sh
@@ -9,6 +9,7 @@ SCHEMA_URL="https://github.com/stans-robot-project/mongodb/archive/main.zip"
 stat() {
     if [ $1 -eq 0 ] ; then 
         echo -e "\e[32m Success \e[0m"
+
    else 
         echo -e "\e[31m Failure \e[0m"
     fi
@@ -45,7 +46,7 @@ curl -s -L -o /tmp/mongodb.zip $SCHEMA_URL
 stat $?
 
 echo -n "Extracting $COMPONENET"
-unzip -o /tmp/${COMPONENT}.zip   &>> $LOGFILE
+unzip -o /tmp/mongodb.zip   &>> $LOGFILE
 stat $?
 
 echo -n "Injecting Schema :"
