01a909548305047d3ca58196eaae46315952e41b
Gopi Pavan <thotagopipavankumar@gmail.com>
2024-01-23 18:42:09 +0530

    B56 Auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-echo -e "***** \e[33m Configuring catalogue \e[0m *****"
+USER_ID=$(id -u)
+COMPONENT=catalogue
+LOGFILE="/tmp/${COMPONENT}.log"
 
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m Success \e[0m"
+   else 
+        echo -e "\e[31m Failure \e[0m"
+    fi
+}
+
+if [ $USER_ID -ne 0 ] ; then 
+   echo -e "\e[31m This script is expected to be executed with sudo or as a root user\e[0m"
+   echo -e "\e[35m Example Usage: \n\t\t \e[0m sudo bash scriptName componenteName"
+   exit 1
+fi
+
+echo -n "Configuring Nodejs Repo"
+curl --silent --location https://rpm.nodesource.com/pub_16.x | sudo bash -
+stat $?
+
+echo -n "Installing Nodejs :"
+yum install nodejs -y &>>  $LOGFILE
+stat $?
